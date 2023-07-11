58849181b3bc9d0b0cccf0ac595f5f205ea51415
Pabiak <pabiak17@gmail.com>
2023-07-11 22:21:46 +0200

    TM-4 add theme.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
-const App = () => (
-    <span>Aplikacja do zarządzania planowaniem i realizacją zadań</span>
-)
+import React from 'react';
+import styled from 'styled-components';
+import ThemeProvider  from './Theme';
+
+const StyledApp = styled.div`
+    background-color: ${({ theme }) => theme.colors.primary};
+    font-family: ${({ theme }) => theme.fontFamilies.primary};
+    color: ${({ theme }) => theme.colors.white};
+    height: 10rem;
+    width: 10rem;
+`;
+
+const App = () => {
+    return (
+        <ThemeProvider>
+            <StyledApp> Hello World </StyledApp>
+        </ThemeProvider>
+    );
+};
 
 export default App;
